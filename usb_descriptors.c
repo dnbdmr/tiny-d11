3525c8dd81987972883bb097da51544551e521ab
dnbdmr <dnbdmr@gmail.com>
2020-11-07 07:59:19 -0800

    Retabbed

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -34,47 +34,47 @@
  */
 #define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
 #define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
+							_PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
 
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
 tusb_desc_device_t const desc_device =
 {
-    .bLength            = sizeof(tusb_desc_device_t),
-    .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,
+	.bLength            = sizeof(tusb_desc_device_t),
+	.bDescriptorType    = TUSB_DESC_DEVICE,
+	.bcdUSB             = 0x0200,
 
-  #if CFG_TUD_CDC
-    // Use Interface Association Descriptor (IAD) for CDC
-    // As required by USB Specs IAD's subclass must be common class (2) and protocol must be IAD (1)
-    .bDeviceClass       = TUSB_CLASS_MISC,
-    .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
-    .bDeviceProtocol    = MISC_PROTOCOL_IAD,
-  #else
-    .bDeviceClass       = 0x00,
-    .bDeviceSubClass    = 0x00,
-    .bDeviceProtocol    = 0x00,
-  #endif
+#if CFG_TUD_CDC
+	// Use Interface Association Descriptor (IAD) for CDC
+	// As required by USB Specs IAD's subclass must be common class (2) and protocol must be IAD (1)
+	.bDeviceClass       = TUSB_CLASS_MISC,
+	.bDeviceSubClass    = MISC_SUBCLASS_COMMON,
+	.bDeviceProtocol    = MISC_PROTOCOL_IAD,
+#else
+	.bDeviceClass       = 0x00,
+	.bDeviceSubClass    = 0x00,
+	.bDeviceProtocol    = 0x00,
+#endif
 
-    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+	.bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor           = 0xCafe,
-    .idProduct          = USB_PID,
-    .bcdDevice          = 0x0100,
+	.idVendor           = 0xCafe,
+	.idProduct          = USB_PID,
+	.bcdDevice          = 0x0100,
 
-    .iManufacturer      = 0x01,
-    .iProduct           = 0x02,
-    .iSerialNumber      = 0x03,
+	.iManufacturer      = 0x01,
+	.iProduct           = 0x02,
+	.iSerialNumber      = 0x03,
 
-    .bNumConfigurations = 0x01
+	.bNumConfigurations = 0x01
 };
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
 uint8_t const * tud_descriptor_device_cb(void)
 {
-  return (uint8_t const *) &desc_device;
+	return (uint8_t const *) &desc_device;
 }
 
 //--------------------------------------------------------------------+
@@ -84,8 +84,8 @@ uint8_t const * tud_descriptor_device_cb(void)
 
 uint8_t const desc_hid_report[] =
 {
-  TUD_HID_REPORT_DESC_KEYBOARD( HID_REPORT_ID(REPORT_ID_KEYBOARD), ),
-  TUD_HID_REPORT_DESC_MOUSE   ( HID_REPORT_ID(REPORT_ID_MOUSE), )
+	TUD_HID_REPORT_DESC_KEYBOARD( HID_REPORT_ID(REPORT_ID_KEYBOARD), ),
+	TUD_HID_REPORT_DESC_MOUSE   ( HID_REPORT_ID(REPORT_ID_MOUSE), )
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
@@ -93,7 +93,7 @@ uint8_t const desc_hid_report[] =
 // Descriptor contents must exist long enough for transfer to complete
 uint8_t const * tud_hid_descriptor_report_cb(void)
 {
-  return desc_hid_report;
+	return desc_hid_report;
 }
 
 #endif
@@ -105,51 +105,51 @@ uint8_t const * tud_hid_descriptor_report_cb(void)
 enum
 {
 #if CFG_TUD_CDC
-  ITF_NUM_CDC = 0,
-  ITF_NUM_CDC_DATA,
+	ITF_NUM_CDC = 0,
+	ITF_NUM_CDC_DATA,
 #endif
 
 #if CFG_TUD_MSC
-  ITF_NUM_MSC,
+	ITF_NUM_MSC,
 #endif
 
 #if CFG_TUD_HID
-  ITF_NUM_HID,
+	ITF_NUM_HID,
 #endif
 
-  ITF_NUM_TOTAL
+	ITF_NUM_TOTAL
 };
 
 #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + CFG_TUD_CDC*TUD_CDC_DESC_LEN + CFG_TUD_MSC*TUD_MSC_DESC_LEN + CFG_TUD_HID*TUD_HID_DESC_LEN)
 
 #if CFG_TUSB_MCU == OPT_MCU_LPC175X_6X || CFG_TUSB_MCU == OPT_MCU_LPC177X_8X || CFG_TUSB_MCU == OPT_MCU_LPC40XX
-  // LPC 17xx and 40xx endpoint type (bulk/interrupt/iso) are fixed by its number
-  // 0 control, 1 In, 2 Bulk, 3 Iso, 4 In etc ...
-  // Note: since CDC EP ( 1 & 2), HID (4) are spot-on, thus we only need to force
-  // endpoint number for MSC to 5
-  #define EPNUM_MSC   0x05
+// LPC 17xx and 40xx endpoint type (bulk/interrupt/iso) are fixed by its number
+// 0 control, 1 In, 2 Bulk, 3 Iso, 4 In etc ...
+// Note: since CDC EP ( 1 & 2), HID (4) are spot-on, thus we only need to force
+// endpoint number for MSC to 5
+#define EPNUM_MSC   0x05
 #else
-  #define EPNUM_MSC   0x03
+#define EPNUM_MSC   0x03
 #endif
 
 uint8_t const desc_configuration[] =
 {
-  // interface count, string index, total length, attribute, power in mA
-  TUD_CONFIG_DESCRIPTOR(ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+	// interface count, string index, total length, attribute, power in mA
+	TUD_CONFIG_DESCRIPTOR(ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
 #if CFG_TUD_CDC
-  // Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-  TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, 0x81, 8, 0x02, 0x82, 64),
+	// Interface number, string index, EP notification address and size, EP data address (out, in) and size.
+	TUD_CDC_DESCRIPTOR(ITF_NUM_CDC, 4, 0x81, 8, 0x02, 0x82, 64),
 #endif
 
 #if CFG_TUD_MSC
-  // Interface number, string index, EP Out & EP In address, EP size
-  TUD_MSC_DESCRIPTOR(ITF_NUM_MSC, 5, EPNUM_MSC, 0x80 | EPNUM_MSC, 64), // highspeed 512
+	// Interface number, string index, EP Out & EP In address, EP size
+	TUD_MSC_DESCRIPTOR(ITF_NUM_MSC, 5, EPNUM_MSC, 0x80 | EPNUM_MSC, 64), // highspeed 512
 #endif
 
 #if CFG_TUD_HID
-  // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
-  TUD_HID_DESCRIPTOR(ITF_NUM_HID, 6, HID_PROTOCOL_NONE, sizeof(desc_hid_report), 0x84, 16, 10)
+	// Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
+	TUD_HID_DESCRIPTOR(ITF_NUM_HID, 6, HID_PROTOCOL_NONE, sizeof(desc_hid_report), 0x84, 16, 10)
 #endif
 };
 
@@ -159,8 +159,8 @@ uint8_t const desc_configuration[] =
 // Descriptor contents must exist long enough for transfer to complete
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 {
-  (void) index; // for multiple configurations
-  return desc_configuration;
+	(void) index; // for multiple configurations
+	return desc_configuration;
 }
 
 //--------------------------------------------------------------------+
@@ -170,13 +170,13 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 // array of pointer to string descriptors
 char const* string_desc_arr [] =
 {
-  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
-  "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB D11",              // 2: Product
-  "123456",                      // 3: Serials, should use chip ID
-  "TinyUSB CDC",                 // 4: CDC Interface
-  "TinyUSB MSC",                 // 5: MSC Interface
-  "TinyUSB HID"                  // 6: HID
+	(const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+	"TinyUSB",                     // 1: Manufacturer
+	"TinyUSB D11",              // 2: Product
+	"123456",                      // 3: Serials, should use chip ID
+	"TinyUSB CDC",                 // 4: CDC Interface
+	"TinyUSB MSC",                 // 5: MSC Interface
+	"TinyUSB HID"                  // 6: HID
 };
 
 static uint16_t _desc_str[33];
@@ -185,45 +185,45 @@ static uint16_t _desc_str[33];
 // Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
 {
-  (void) langid;
+	(void) langid;
 
-  uint8_t chr_count;
+	uint8_t chr_count;
 
-  if (index == 0) { // Language id
-    memcpy(&_desc_str[1], string_desc_arr[0], 2);
-    chr_count = 1;
-  } else if (index == 3) { // Serial number
-	  uint32_t* addresses[4] = {(uint32_t *) 0x0080A00C, (uint32_t *) 0x0080A040,
-		  (uint32_t *) 0x0080A044, (uint32_t *) 0x0080A048};
+	if (index == 0) { // Language id
+		memcpy(&_desc_str[1], string_desc_arr[0], 2);
+		chr_count = 1;
+	} else if (index == 3) { // Serial number
+		uint32_t* addresses[4] = {(uint32_t *) 0x0080A00C, (uint32_t *) 0x0080A040,
+			(uint32_t *) 0x0080A044, (uint32_t *) 0x0080A048};
 
-	  chr_count = 0;
+		chr_count = 0;
 
-	  for (uint8_t i = 0; i < 4; ++i) { // cycle through addresses
-		  for (int8_t sh = 28; sh >= 0; sh -= 4) { // cycle through nibbles
-			  uint8_t d = (*(addresses[i]) >> sh) & 0xf;
-			  _desc_str[1 + chr_count] = d > 9 ? 'A' + d - 10 : '0' + d;
-			  chr_count++;
-		  }
-	  }
-  } else {
-    // Convert ASCII string into UTF-16
+		for (uint8_t i = 0; i < 4; ++i) { // cycle through addresses
+			for (int8_t sh = 28; sh >= 0; sh -= 4) { // cycle through nibbles
+				uint8_t d = (*(addresses[i]) >> sh) & 0xf;
+				_desc_str[1 + chr_count] = d > 9 ? 'A' + d - 10 : '0' + d;
+				chr_count++;
+			}
+		}
+	} else {
+		// Convert ASCII string into UTF-16
 
-    if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
+		if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
 
-    const char* str = string_desc_arr[index];
+		const char* str = string_desc_arr[index];
 
-    // Cap at max char
-    chr_count = strlen(str);
-    if ( chr_count > 31 ) chr_count = 31;
+		// Cap at max char
+		chr_count = strlen(str);
+		if ( chr_count > 31 ) chr_count = 31;
 
-    for(uint8_t i=0; i<chr_count; i++)
-    {
-      _desc_str[1+i] = str[i];
-    }
-  }
+		for(uint8_t i=0; i<chr_count; i++)
+		{
+			_desc_str[1+i] = str[i];
+		}
+	}
 
-  // first byte is length (including header), second byte is string type
-  _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+	// first byte is length (including header), second byte is string type
+	_desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
 
-  return _desc_str;
+	return _desc_str;
 }
